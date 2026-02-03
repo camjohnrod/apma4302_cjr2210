eed7bc1085bc4e543af26970343ee6a7636ea109
Cameron Rodriguez <camjohnrod@gmail.com>
2026-02-02 23:14:06 -0500

    Got a working e(x) function using Horner's method (even # of Taylor terms per processor)

--- hw1/expx.c
+++ hw1/expx.c
@@ -1,23 +1,40 @@
 #include <petsc.h>
+#include <petscdt.h>
+#include <math.h>
 
 int main(int argc, char **argv) {
-  PetscMPIInt    rank;
-  PetscInt       i;
-  PetscReal      x = 1.0, localval, globalsum;
+  PetscMPIInt    rank, num_processes, terms_per_rank;
+  PetscInt       N = 3, i, num_remainder;
+  PetscReal      x = 1.0, localval, globalsum, factorial_val, true_val, rel_error;
 
   PetscCall(PetscInitialize(&argc,&argv,NULL,
       "Compute exp(x) in parallel with PETSc.\n\n"));
   PetscCall(MPI_Comm_rank(PETSC_COMM_WORLD,&rank));
 
+  PetscCall(MPI_Comm_size(PETSC_COMM_WORLD, &num_processes));
+
   // read option
   PetscOptionsBegin(PETSC_COMM_WORLD,"","options for expx","");
   PetscCall(PetscOptionsReal("-x","input to exp(x) function",NULL,x,&x,NULL));
+  PetscCall(PetscOptionsInt("-N","number of Taylor series terms to use",NULL,N,&N,NULL));
   PetscOptionsEnd();
 
-  // compute  x^n/n!  where n = (rank of process) + 1
-  localval = 1.0;
-  for (i = 1; i < rank+1; i++)
-      localval *= x/i;
+  // compute e^x using N Taylor series terms (Horner's Method)
+  terms_per_rank = N / num_processes;
+  num_remainder = N - terms_per_rank * num_processes;
+  PetscCall(PetscPrintf(PETSC_COMM_WORLD,"Taylor series terms per rank: %d\n", terms_per_rank));
+  PetscCall(PetscPrintf(PETSC_COMM_WORLD,"Number of remaining terms: %d\n", num_remainder));
+
+  localval = 0.0;
+  for (i = terms_per_rank - 1; i >= 0; i--) {
+    PetscDTFactorial(terms_per_rank * rank + i, &factorial_val);
+    localval += 1 / factorial_val;
+    if (i != 0) {
+        localval *= x;   
+    }
+  }
+    
+  localval *= PetscPowReal(x, (terms_per_rank * rank));
 
   // sum the contributions over all processes
   PetscCall(MPI_Allreduce(&localval,&globalsum,1,MPIU_REAL,MPIU_SUM,
@@ -26,8 +43,12 @@ int main(int argc, char **argv) {
   // output estimate and report on work from each process
   PetscCall(PetscPrintf(PETSC_COMM_WORLD,
       "exp(%17.15f) is about %17.15f\n",x,globalsum));
-  PetscCall(PetscPrintf(PETSC_COMM_SELF,
-      "rank %d did %d flops\n",rank,(rank > 0) ? 2*rank : 0));
+
+  true_val = exp(x);
+  rel_error = PetscAbsReal(((true_val - globalsum) / true_val)) / PETSC_MACHINE_EPSILON;
+  PetscCall(PetscPrintf(PETSC_COMM_WORLD,
+      "The relative error compared to the exp(x) function is %.5f * epsilon\n", rel_error));
+
   PetscCall(PetscFinalize());
-  return 0;
+  return EXIT_SUCCESS;
 }
